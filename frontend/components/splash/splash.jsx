78f18571829e21fe6f117ac0a85d0854bf9a6825
Carlos Garcia <carlosaicrag@gmail.com>
2020-01-28 18:22:04 -0800

    create splash container and component

---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+class Splash extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                this is the splash page
+            </div>
+        )
+    }
+}
+
+export default Splash
---
+++ frontend/components/splash/splash_container.js
@@ -0,0 +1,16 @@
+import {connect} from "react-redux"
+import Splash from "./splash"
+
+const msp = function(store){
+    return({
+        
+    })
+}
+
+const mdp = function(dispatch){
+    return({
+
+    })
+}
+
+export default connect(msp,mdp)(Splash)
