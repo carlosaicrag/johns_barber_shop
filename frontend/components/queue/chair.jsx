fe49c3b2bc68cfedf8c2531844b81e4638d51155
Carlos Garcia <carlosaicrag@gmail.com>
2020-03-07 19:45:08 -0800

    setup for heroku

--- frontend/components/queue/chair.jsx
+++ frontend/components/queue/chair.jsx
@@ -5,7 +5,7 @@ const Chair = function(props){
     return(
         <div className="chair-container">
             <div className="chair-container-barber-name">{props.barber.fname}</div>
-            {/* <img src={props.barber.image_url} alt="barber_url"/> */}
+            <img src={props.barber.image_url} alt="barber_url"/>
             <img src="/barber-chair.png" alt="barber-chair" />
             <div className="chair-container-barber-name">Time to Wait: 0 mins</div>
         </div>
