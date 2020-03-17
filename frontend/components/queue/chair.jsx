391943d29ad0de8664512e2f6658dd2c8858ff9b
lina kherchi <linakherchi@gmail.com>
2020-03-17 15:39:39 -0700

    barbershop page in progress

--- frontend/components/queue/chair.jsx
+++ frontend/components/queue/chair.jsx
@@ -5,10 +5,8 @@ const Chair = function(props){
     
     return(
        <>
-        <div onClick={() => props.history.push(`./queue/${props.barber.id}`)}></div>
-            <div>{props.barber.fname}</div>
         <div className="chair-container">
-            <div className="chair-container-barber-name">{props.barber.fname}</div>
+            <div onClick={() => props.history.push(`./queue/${props.barber.id}`)}className="chair-container-barber-name">{props.barber.fname}</div>
             <img src={props.barber.image_url} alt="barber_url"/>
             <img src="/barber-chair.png" alt="barber-chair" />
             <div className="chair-container-barber-name">Time to Wait: 0 mins</div>
