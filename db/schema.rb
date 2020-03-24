37722146466ba11451b06c90156091188dc222fb
Carlos Garcia <carlosaicrag@gmail.com>
2020-03-24 16:52:09 -0700

    create client_haircut_avg_times table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_053644) do
+ActiveRecord::Schema.define(version: 2020_03_24_234650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 2020_03_24_053644) do
     t.integer "avg_time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["client_id", "haircut_id"], name: "index_client_haircut_avg_times_on_client_id_and_haircut_id", unique: true
+    t.integer "barber_id", null: false
+    t.index ["barber_id"], name: "index_client_haircut_avg_times_on_barber_id"
     t.index ["client_id"], name: "index_client_haircut_avg_times_on_client_id"
     t.index ["haircut_id"], name: "index_client_haircut_avg_times_on_haircut_id"
   end
