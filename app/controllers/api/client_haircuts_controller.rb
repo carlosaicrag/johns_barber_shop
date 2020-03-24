2c959f6f8f307559d3732aac6d58a0b01640c672
lina kherchi <linakherchi@gmail.com>
2020-03-24 06:24:20 -0700

    no more testing in queue controller

--- app/controllers/api/client_haircuts_controller.rb
+++ app/controllers/api/client_haircuts_controller.rb
@@ -1,8 +1,7 @@
 class Api::ClientHaircutsController < ApplicationController
     def queue
         # debugger
-        # @client_haircuts = ClientHaircut.where(barber_id: current_user.id).where(closed_at: [nil])
-        @client_haircuts = ClientHaircut.where(barber_id: 45).where(closed_at: [nil]).order('created_at DESC') #for testing
+        @client_haircuts = ClientHaircut.where(barber_id: current_user.id).where(closed_at: [nil]).order('created_at DESC')
         render :queue
     end
 end
