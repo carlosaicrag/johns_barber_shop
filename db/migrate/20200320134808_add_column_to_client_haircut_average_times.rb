3e5a8ae61ab364f0cc1a631ccc84448a55f1cf6b
Carlos Garcia <carlosaicrag@gmail.com>
2020-03-29 20:41:25 -0700

    fix migration

--- db/migrate/20200320134808_add_column_to_client_haircut_average_times.rb
+++ db/migrate/20200320134808_add_column_to_client_haircut_average_times.rb
@@ -1,11 +1,9 @@
 class AddColumnToClientHaircutAverageTimes < ActiveRecord::Migration[5.2]
   def change
     create_table :client_haircut_avg_times do |t|
-      t.integer :client_id, :integer, null:false
-      t.integer :haircut_id,:integer, null:false
-      t.integer :barber_id, :integer, null:false
+      t.integer :client_id, null:false
+      t.integer :haircut_id,null:false
+      t.integer :barber_id, null:false
       t.integer :avg_time, default: 45
       t.timestamps
     end
-  end
-end
