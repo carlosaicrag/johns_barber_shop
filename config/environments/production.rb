1fbd1cc09a68a4dfcf68eb5ee15a076960cebaa1
Carlos Garcia <carlosaicrag@gmail.com>
2020-03-30 11:46:25 -0700

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_mailer.default_url_options = { :host => 'https://johns-barber-shop.herokuapp.com/' }
+  # config.action_mailer.default_url_options = { :host => 'https://johns-barber-shop.herokuapp.com/' }
 
   routes.default_url_options[:host] = 'https://johns-barber-shop.herokuapp.com/'
 
