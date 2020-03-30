574f3b3b29ed548674ae0aa73a9836e3ab47561a
Julia Wang <juliawang105@gmail.com>
2020-03-30 14:42:12 -0700

    add gif

--- frontend/components/error_pages/unauthorized.jsx
+++ frontend/components/error_pages/unauthorized.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Unauthorized(){
   return(
-    <div>Unauthorized</div>
+    <div className="unauthorized">
+      <h1 className="warning_text">
+        This page is not meant for your eyes! 
+            </h1>
+      <div>
+        <img className="gif" src="./no_hair.gif" alt="" />
+      </div>
+
+    </div>
   )
 };
 
