d4e7af2f335b98b4d3aadc1fd75c4b6e5b4363ac
Carlos Garcia <carlosaicrag@gmail.com>
2020-07-23 20:19:24 -0700

    create specs for clients controller

--- spec/controllers/api/clients_controller_spec.rb
+++ spec/controllers/api/clients_controller_spec.rb
@@ -1,7 +1,37 @@
-require 'test_helper'
+require 'rails_helper'
 
-class Api::ClientsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe Api::ClientsController, type: :controller do 
+  let!(:client) {Client.create(password: "password", email: "luke@gmail.com", fname: "Carlos", lname: "Garcia")}  
+
+  describe "POST create" do 
+    it "succesfully saves client to database with valid credentials" do 
+      post :create, params: {client: {password: "password", email: "John@gmail.com", fname: "John", lname: "Doe" }}, format: :json
+      client = Client.find_by(fname: "John")
+      expect(response.status).to eq(200)
+      expect(client).to_not be(nil)
+    end
+
+    it "renders errors when client has invalid credentials" do 
+      post :create, params: {client: {password: "password", email: "John@gmail.com", fname: "John"}}, format: :json
+      expect(response.status).to eq(422)
+      expect(JSON(response.body)).to_not be_empty
+    end
+  end
+
+  describe "GET show" do
+    context "when the user is found" do 
+      it "should render a level 200 status" do 
+        get :show, params: {id: client.id}, format: :json
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context "when the user is not found" do 
+      it "should render a level 422 status and error message" do 
+        get :show, params: {id: -1}, format: :json
+        expect(response.status).to eq(422)
+        expect(JSON(response.body)[0]).to eq("No client found with that id")
+      end
+    end
+  end
 end
