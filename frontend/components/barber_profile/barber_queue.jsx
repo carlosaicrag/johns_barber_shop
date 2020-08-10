5a70adf9bdf34bba156419fb0a0d147bb66eb259
Carlos Garcia <carlosaicrag@gmail.com>
2020-08-10 15:04:16 -0700

    fix close a client bug

--- frontend/components/barber_profile/barber_queue.jsx
+++ frontend/components/barber_profile/barber_queue.jsx
@@ -1,76 +1,76 @@
 import React from "react";
 
-class BarberQueue extends React.Component{
-  constructor(props){
+class BarberQueue extends React.Component {
+  constructor(props) {
     super(props)
-    
+
     this.takeOrReleaseClient = this.takeOrReleaseClient.bind(this)
-    if (localStorage.getItem("minutes")){
-      this.state = { minutes: parseInt(localStorage.getItem("minutes")), seconds: parseInt(localStorage.getItem("seconds")),cuttingHair:false}
-    }else{
+    if (localStorage.getItem("minutes")) {
+      this.state = { minutes: parseInt(localStorage.getItem("minutes")), seconds: parseInt(localStorage.getItem("seconds")), cuttingHair: false }
+    } else {
       this.state = { minutes: 0, seconds: 0, cuttingHair: false }
     }
   }
-  
-  tick(){
+
+  tick() {
     let that = this
     localStorage.setItem("minutes", this.props.timeElapsed.mins)
     localStorage.setItem("seconds", this.props.timeElapsed.seconds)
     this.stopWatch = setInterval(() => {
-      localStorage.setItem("seconds", parseInt(localStorage.getItem("seconds"))+1)
-      if (parseInt(localStorage.getItem("seconds")) === 60){
+      localStorage.setItem("seconds", parseInt(localStorage.getItem("seconds")) + 1)
+      if (parseInt(localStorage.getItem("seconds")) === 60) {
         localStorage.setItem("seconds", 0)
         localStorage.setItem("minutes", parseInt(that.state.minutes) + 1)
       }
-      this.setState({minutes: localStorage.getItem("minutes"), seconds: localStorage.getItem("seconds"),cuttingHair:true})
-    },1000)
+      this.setState({ minutes: localStorage.getItem("minutes"), seconds: localStorage.getItem("seconds"), cuttingHair: true })
+    }, 1000)
   }
-  
-  takeOrReleaseClient(){
-    if(this.cuttingHair()){
+
+  takeOrReleaseClient() {
+    if (this.cuttingHair()) {
       clearInterval(this.stopWatch)
       localStorage.clear()
-      this.setState({minutes:0, seconds:0, cuttingHair:true},
-        () =>{
+      this.setState({ minutes: 0, seconds: 0, cuttingHair: true },
+        () => {
           this.props.closeClientHaircut(this.props.clientHaircutId)
         })
       return null
     }
 
-    if(this.props.numberOfPeopleWaiting > 0 ){
-      this.setState({cuttingHair:false},
-        () =>{
+    if (this.props.numberOfPeopleWaiting > 0) {
+      this.setState({ cuttingHair: false },
+        () => {
           this.props.updateBarberWorkingStatus(this.props.barberInfo)
         })
-    }else{
+    } else {
       return null
     }
   }
 
-  inProgressCSS(inProgressHTMLCollection){
-    if (inProgressHTMLCollection.style.opacity === "0.7" || inProgressHTMLCollection.style.opacity === ""){
+  inProgressCSS(inProgressHTMLCollection) {
+    if (inProgressHTMLCollection.style.opacity === "0.7" || inProgressHTMLCollection.style.opacity === "") {
       inProgressHTMLCollection.style.opacity = "1"
-    }else{
+    } else {
       inProgressHTMLCollection.style.opacity = "0.7"
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchQueue()
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     clearInterval(this.stopWatch)
   }
 
-  startTiltingProgress(){
+  startTiltingProgress() {
     const inProgress = document.getElementsByClassName("in-progress-one")
-    this.intervalProgressId = setInterval(() => this.inProgressCSS(inProgress[0]), 500 )
+    this.intervalProgressId = setInterval(() => this.inProgressCSS(inProgress[0]), 500)
   }
 
-  cuttingHair(){
+  cuttingHair() {
     if (this.props.booleanCuttingHair) {
-      if(!this.state.cuttingHair){
+      if (!this.state.cuttingHair) {
         this.tick()
       }
       return true
@@ -79,25 +79,25 @@ class BarberQueue extends React.Component{
     }
   }
 
-  render(){
+  render() {
     const barberName = this.props.barberName
     const splittedBarberName = (("Chez ").split("").concat(barberName.split("")));
     // if(!this.props.clientHaircutId){
     //   return null
     // }
     // this.cuttingHair()
-    return(
+    return (
       <div>
         <div className="chez-barber-name">
           {splittedBarberName.join('')}
         </div>
         <div className="in-progress">
           <div className="in-progress-one">
-            {this.props.booleanCuttingHair? 'In progress..': 'Take client'}
+            {this.props.booleanCuttingHair ? 'In progress..' : 'Take client'}
           </div>
-  
+
           <div className="release-button">
-            <img src={window.circleDone} alt="person-done" height="30" onClick={this.takeOrReleaseClient}/>
+            <img src={window.circleDone} alt="person-done" height="30" onClick={this.takeOrReleaseClient} />
           </div>
         </div>
         <div className="barber-chair">
@@ -110,21 +110,21 @@ class BarberQueue extends React.Component{
           <div className="number-of-people-waiting">
             {this.props.numberOfPeopleWaiting}
           </div>
-          <img src={window.barberPole1} height="130" width="120"/>
+          <img src={window.barberPole1} height="130" width="120" />
           <div className="client-information">
             <div className="client-information-1">
-              <img src={window.barber} width="50" height="50" className="client-img"/>
+              <img src={window.barber} width="50" height="50" className="client-img" />
               <div className="client-name">{(this.props.clientFirstName + ' ' + this.props.clientLastName)}</div>
             </div>
             <div className="client-information-2">
-              <img src={window.scissors} width="50" height="50" className="scissors-img"/>
+              <img src={window.scissors} width="50" height="50" className="scissors-img" />
               <div className="haircut-name">{this.props.clientHaircut}</div>
             </div>
           </div>
         </div>
       </div>
     )
-  }  
+  }
 }
 export default BarberQueue
 
